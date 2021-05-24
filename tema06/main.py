584149e44fca9f064b96313de9c2823ff3992940
ibragionp <isabellabpereira@protonmail.com>
2021-05-24 10:50:45 -0300

    desfazendo alterações main.py

--- tema06/main.py
+++ tema06/main.py
@@ -6,27 +6,12 @@ Created on Wed May 19 17:10:33 2021
 @author: ilegra
 """
 import subprocess
-import sys
+import os
 
 sync_aws_str = 'aws s3 sync ~/devops-and-cloud-basics/tema06/output/ s3://jt-dataeng-isabellabragionpereira/tema09/output/'
 imdb_analysis_file = '/imdb_analysis.py'
 twitter_analysis_file = '/twitter_analysis.py'
-package_lst = ['datetime', 'mysql-connector-python', 'os', 'pandas', 
-               'pandas.io', 'time', 'tweepy']
-
-def install_packages(package_lst):
-    for package in package_lst:
-        subprocess.check_call([sys.executable, 
-                               "sudo",
-                               "-m", 
-                               "pip", 
-                               "install", 
-                               package])
     
-install_packages(package_lst)
-    
-import os
-
 def execute_scripts_analysis():
     path = os.path.dirname(os.path.realpath(__file__))
     exec(open(path + imdb_analysis_file).read())
@@ -38,6 +23,5 @@ def sync_bucket_s3_aws():
     push.wait() 
     print(push.returncode)
     
-install_packages(package_lst)
 execute_scripts_analysis()
 sync_bucket_s3_aws()
