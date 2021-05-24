64ef846613bc85d2008fbe43d915feec6c5c4ef2
ibragionp <isabellabpereira@protonmail.com>
2021-05-24 12:02:08 -0300

    alterações imports libraries

--- tema06/main.py
+++ tema06/main.py
@@ -6,22 +6,22 @@ Created on Wed May 19 17:10:33 2021
 @author: ilegra
 """
 from subprocess import Popen, PIPE
-from os import path
+#from os import path
+import imdb_analysis.py
+import twitter_analysis.py
 
 sync_aws_str = 'aws s3 sync ~/devops-and-cloud-basics/tema06/output/ s3://jt-dataeng-isabellabragionpereira/tema09/output/'
-imdb_analysis_file = '/imdb_analysis.py'
-twitter_analysis_file = '/twitter_analysis.py'
     
 def execute_scripts_analysis():
-    files_path = path.dirname(path.realpath(__file__))
-    exec(open(files_path + imdb_analysis_file).read())
-    exec(open(files_path + twitter_analysis_file).read())
-
+    imdb_analysis.py
+    twitter_analysis.py
+    
 def sync_bucket_s3_aws():
     terminal = sync_aws_str
     push = Popen(terminal, shell = True, stdout = PIPE)
     push.wait() 
     print(push.returncode)
+ 
     
 execute_scripts_analysis()
 sync_bucket_s3_aws()
