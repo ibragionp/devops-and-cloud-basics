d5e5f4dacb42248cf9ce3855b75070795b4a5f2d
ibragionp <isabellabpereira@protonmail.com>
2021-05-25 15:48:02 -0300

    alterações no main.py para não execução da análise imdb

--- tema06/main.py
+++ tema06/main.py
@@ -6,14 +6,13 @@ Created on Wed May 19 17:10:33 2021
 @author: ilegra
 """
 from subprocess import Popen, PIPE
-#from os import path
 import imdb_analysis.py
 import twitter_analysis.py
 
 sync_aws_str = 'aws s3 sync ~/devops-and-cloud-basics/tema06/output/ s3://jt-dataeng-isabellabragionpereira/tema09/output/'
     
 def execute_scripts_analysis():
-    imdb_analysis.py
+    #imdb_analysis.py
     twitter_analysis.py
     
 def sync_bucket_s3_aws():
