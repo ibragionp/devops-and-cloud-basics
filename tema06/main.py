1662cfcad08c44cf2f71943c1f9e05a68cbc2942
ibragionp <isabellabpereira@protonmail.com>
2021-05-25 16:53:09 -0300

    Alterações main.py

--- tema06/main.py
+++ tema06/main.py
@@ -6,12 +6,13 @@ Created on Wed May 19 17:10:33 2021
 @author: ilegra
 """
 from subprocess import Popen, PIPE
+import twitter_analysis
 
 sync_aws_str = 'aws s3 sync ~/devops-and-cloud-basics/tema06/output/ s3://jt-dataeng-isabellabragionpereira/tema09/output/'
     
 def execute_scripts_analysis():
-    #exec(open('imdb_analysis.py').read())
-    exec(open('twitter_analysis.py').read())
+    #imdb_analysis.py
+    twitter_analysis.py
     
 def sync_bucket_s3_aws():
     terminal = sync_aws_str
