4042551a7855ae537599a7a69db7ca78e2246114
ibragionp <isabellabpereira@protonmail.com>
2021-05-25 16:45:41 -0300

    Alterações gitgnore

--- tema06/main.py
+++ tema06/main.py
@@ -6,14 +6,12 @@ Created on Wed May 19 17:10:33 2021
 @author: ilegra
 """
 from subprocess import Popen, PIPE
-#import imdb_analysis.py
-import twitter_analysis.py
 
 sync_aws_str = 'aws s3 sync ~/devops-and-cloud-basics/tema06/output/ s3://jt-dataeng-isabellabragionpereira/tema09/output/'
     
 def execute_scripts_analysis():
-    #imdb_analysis.py
-    twitter_analysis.py
+    #exec(open('imdb_analysis.py').read())
+    exec(open('twitter_analysis.py').read())
     
 def sync_bucket_s3_aws():
     terminal = sync_aws_str
