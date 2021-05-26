2b6e94bceb61cd9d7eb4463b351e673a3e179227
ibragionp <isabellabpereira@protonmail.com>
2021-05-26 09:23:34 -0300

    Comentando imdb_analysis

--- tema06/main.py
+++ tema06/main.py
@@ -6,12 +6,13 @@ Created on Wed May 19 17:10:33 2021
 @author: ilegra
 """
 from subprocess import Popen, PIPE
+#import imdb_analysis
 import twitter_analysis
 
 sync_aws_str = 'aws s3 sync ~/devops-and-cloud-basics/tema06/output/ s3://jt-dataeng-isabellabragionpereira/tema09/output/'
     
 def execute_scripts_analysis():
-    #imdb_analysis.py
+    #imdb_analysis
     twitter_analysis
     
 def sync_bucket_s3_aws():
