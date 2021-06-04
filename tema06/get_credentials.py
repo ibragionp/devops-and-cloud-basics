7a0b2dbd7ee77bbe8df02741d50202d25809856e
ibragionp <isabellabpereira@protonmail.com>
2021-06-04 10:43:12 -0300

    teste script get credentials

---
+++ tema06/get_credentials.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  4 10:40:12 2021
+
+@author: ilegra
+"""
+
+from botocore.credentials import InstanceMetadataProvider, InstanceMetadataFetcher
+
+provider = InstanceMetadataProvider(iam_role_fetcher=InstanceMetadataFetcher(timeout=1000, num_attempts=2))
+credentials = provider.load()
+
+access_key = credentials.access_key
+secret_key = credentials.secret_key
+
+print(access_key)
+print(secret_key)
