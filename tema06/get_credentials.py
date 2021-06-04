a6f1a25ab3ca7bc52066994ea0c08fe74d6e38d7
ibragionp <isabellabpereira@protonmail.com>
2021-06-04 10:52:40 -0300

    teste script get credentials

--- tema06/get_credentials.py
+++ tema06/get_credentials.py
@@ -6,15 +6,24 @@ Created on Fri Jun  4 10:40:12 2021
 @author: ilegra
 """
 
+'''
+
 from botocore.credentials import InstanceMetadataProvider, InstanceMetadataFetcher
 
 provider = InstanceMetadataProvider(iam_role_fetcher=InstanceMetadataFetcher(timeout=1000, num_attempts=2))
 credentials = provider.load()
-'''
+
+
 access_key = credentials.access_key
 secret_key = credentials.secret_key
 
 print(access_key)
 print(secret_key)
 '''
-print(credentials)
+
+
+from botocore.credentials import InstanceMetadataProvider, InstanceMetadataFetcher
+
+provider = InstanceMetadataProvider(iam_role_fetcher=InstanceMetadataFetcher(timeout=1000, num_attempts=2))
+creds = provider.load().get_frozen_credentials()
+print(creds)
