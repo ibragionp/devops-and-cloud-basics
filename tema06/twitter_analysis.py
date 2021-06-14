a074d49bed271c34aff4472555a83f1f2d281af6
ibragionp <isabellabpereira@protonmail.com>
2021-06-14 15:48:03 -0300

    alterações finais twitter_analysis.py

--- tema06/twitter_analysis.py
+++ tema06/twitter_analysis.py
@@ -24,22 +24,7 @@ actor_actress_col = 'Actor/actress name'
 
 date_since = '2010-01-01'
 quantity = 10
-'''
-from boto3 import Session
 
-session = Session()
-credentials = session.get_credentials()
-print(credentials)
-# Credentials are refreshable, so accessing your access key / secret key
-# separately can lead to a race condition. Use this to get an actual matched
-# set.
-current_credentials = credentials.get_frozen_credentials()
-
-# I would not recommend actually printing these. Generally unsafe.
-print(current_credentials.access_key)
-print(current_credentials.secret_key)
-print(current_credentials.token)
-'''
 path = os.path.dirname(os.path.realpath(__file__))
 
 def api_connection():
