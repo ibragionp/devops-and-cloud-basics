c834c079046bdb7c9a8afb0e2e1f07648336584d
ibragionp <isabellabpereira@protonmail.com>
2021-06-14 17:22:04 -0300

    alterações no teste

---
+++ tema06/main_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 16:03:53 2021
+
+@author: ilegra
+"""
+
+import os
+
+output_path = '/output'
+top_actors_tweets_file = '/top_actors_file.csv'
+top_actors_tweets_file = '/top_actors_tweets_file.csv'
+
+files_path = os.path.dirname(os.path.realpath(__file__)) + output_path
+
+def check_file_exists():
+    exists = 'Test failed! Files were not generated·' 
+    if os.path.isfile(files_path + top_actors_tweets_file) and os.path.isfile(files_path + top_actors_tweets_file):
+        exists = 'Test passed! Files were generated·' 
+    return exists
+
+print(check_file_exists())
